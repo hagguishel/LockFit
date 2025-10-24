cedef0e87300ff955d5c7417c8172f61218e6b6c
hagguishel <hagguirzt@gmail.com>
2025-10-24 17:21:14 +0200

    mise a jour du docker-entry

--- Application/Backend/docker-entrypoint.sh
+++ Application/Backend/docker-entrypoint.sh
@@ -7,11 +7,11 @@ npx prisma generate
 echo "🗃️  Prisma migrate deploy…"
 npx prisma migrate deploy
 
-# Détecte automatiquement le fichier d'entrée (principal.js ou main.js)
+# On détermine le point d'entrée compilé
 APP_JS="dist/principal.js"
 if [ ! -f "$APP_JS" ]; then
   APP_JS="dist/main.js"
 fi
 
-echo "🚀 Starting NestJS ($APP_JS)…"
+echo "🚀 Starting NestJS on port $PORT with $APP_JS…"
 node "$APP_JS"
